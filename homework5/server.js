8e7c56b00276b39627bf0b8f5f1d9fd7fd3ed148
NguyenThanhHung98 <nthungptit@gmail.com>
2019-03-10 16:22:20 +0700

    .

---
+++ homework5/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+
+const app = express();
+
+const fs = require('fs');
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get('/ask',(req,res) => {
+    res.sendFile(__dirname + "/views/ask.html");
+})
+
+app.get('/',(req,res) => {
+    const answerList = JSON.parse(fs.readFileSync("questions.json"));
+    const answer = answerList[Math.floor(Math.random()*answerList.length)];
+    res.send(
+    '<nav>'+
+        '<a href="/ask">' + 'Hỏi nhanh' + '</a>'+ ' ' + 
+        '<a href="/">' + 'Đáp gọn' + '</a>' +
+    '</nav>' +
+    '<h1 class="text-center" id="text-question" style="margin-left: 175px">' + answer + '</h1>' + 
+    '<button type="submit" name="but_yesno" value="no" id="but_1" style="background-color: red;margin-left: 100px">' + 'Sai / Không / Trái' + '</button>' + 
+    '<button type="submit" name="but_yesno" value="yes" id="but_2" style="background-color: blue;margin-left: 200px">' + 'Đúng / Có / Phải' + '</button>' + 
+    '<br>' + '<br>' +
+    '<button type="submit" name="but_yesno" value="ketquavote" id="but_3" style="margin-left: 225px">' + 'Kết quả vote' + '</button>' + 
+    '<button type="submit" name="but_yesno" value="cauhoikhac" id="but_4">' + 'Câu hỏi khác' + '</button>'
+    );
+
+})
+
+app.post('/addquestion', (req,res) =>{
+    const question = req.body.question;
+    const questionList =JSON.parse(fs.readFileSync('questions.json','utf-8'));
+    questionList.push(question);
+    fs.writeFileSync('questions.json',JSON.stringify(questionList),(err) => {
+        if(err) console.log(err);
+        else console.log("success");
+    })
+    res.redirect('/ask');
+})
+
+app.listen(6969, (err) => {
+    if(err) console.log(err);
+    else console.log('server start success');
+})
